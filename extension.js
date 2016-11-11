2d424af4060b57a2eddb3ad44a0094dfc6100f48
Théodore BIADALA <theodore@biadala.net>
2016-11-11 11:33:12 +0100

    Refactor and use sqlite

--- extension.js
+++ extension.js
@@ -3,79 +3,186 @@
  */
 'use strict';
 
-const GnomeSession = imports.misc.gnomeSession;
-const Gio = imports.gi.Gio;
-const GLib = imports.gi.GLib;
+// User configuration
+const config = {
+  dbName: '.windowtraking.sqlite',
+  dbDir: imports.gi.GLib.get_home_dir(),
+};
 
-let focusCallbackID = 0;
-let activeWindow = null;
-let awCallbackID = 0;
+// Custom code
+const logData = (function (Gda, config) {
 
-let filePath;
-let logFile;
-let logFileStream;
-let presence;
+  let lastTime = null;
 
-function logData() {
-  const win = global.display.focus_window;
-  if (!win) { return; }
+  let connection = null;
 
-  writeData({
-    date: (new Date).toISOString(),
-    origin: 'window',
-    type: win.get_wm_class(),
-    title: win.get_title()
-  });
-}
-
-function writeData(data) {
-  log(JSON.stringify(data));
-  logFileStream.write(JSON.stringify(data) + "\n", null);
-}
-
-function onWindowChange() {
-  const win = global.display.focus_window;
-  if (activeWindow) {
-    activeWindow.disconnect(awCallbackID);
+  function log(rawData) {
+    if (!connection) {
+      connect();
+    }
+    // This is not a copy but it should be!
+    const data = rawData;
+    const time = new Date();
+    data.date = time.toISOString();
+    data.timezone = time.toString().match(/\(([\w]{3,4})\)/)[1] || 'CET';
+    if (lastTime) {
+      // Store duration as seconds.
+      data.duration = (time - lastTime) / 1000;
+    }
+    write.apply(this, buildData(data));
+    lastTime = time;
   }
-  if (win) {
-     if(win !== activeWindow) {
-       activeWindow = win;
-       awCallbackID = win.connect('notify::title', logData);
-     }
-    logData();
-  }
-}
 
-function onPresenceChange() {
-  writeData({
-    date: (new Date).toISOString(),
-    origin: 'presence',
-    status: ['idle', 'invisible', 'busy', 'available'][presence.status],
-  });
-  if (focusCallbackID) {
-    global.display.disconnect(focusCallbackID);
+  function buildData(rawData) {
+    const keys = ['date', 'timezone', 'type', 'key', 'value', 'duration', 'origin'];
+    const def = {
+      type: '',
+      key: '',
+      value: '',
+      duration: null,
+      origin: 'monkey'
+    };
+    return [keys, keys.map(key => rawData[key] || def[key])];
   }
-  if (presence.status === 3) {
-    logData();
+
+  function write(keys, values) {
+    connection.execute_non_select_command('INSERT INTO log (' + keys.join(',') + ') VALUES ("' + values.join('","') + '");');
   }
-  focusCallbackID = global.display.connect('notify::focus-window', onWindowChange);
-}
+
+  function connect() {
+    if (!connection) {
+      connection = new Gda.Connection({
+        provider: Gda.Config.get_provider('SQLite'),
+        cnc_string: 'DB_DIR=' + config.dbDir + ';DB_NAME=' + config.dbName
+      });
+      connection.open();
+      initializeDb();
+    }
+  }
+
+
+  function initializeDb() {
+    try {
+      connection.execute_select_command('SELECT id FROM log LIMIT 1');
+    }
+    catch (e) {
+      connection.execute_non_select_command(['CREATE TABLE log (',
+        'id INTEGER PRIMARY KEY,',
+        'date TEXT NOT NULL,',
+        'timezone TEXT NOT NULL,',
+        'type TEXT NOT NULL,',
+        'key TEXT NOT NULL,',
+        'value TEXT NOT NULL,',
+        'duration INTEGER NULL,',
+        'origin TEXT NOT NULL',
+        ')'].join('\n'));
+      connection.execute_non_select_command('CREATE INDEX date_timezone ON log(date, timezone)');
+      connection.execute_non_select_command('CREATE INDEX type_key_origin ON log(type, key, origin)');
+      connection.execute_non_select_command('CREATE INDEX duration ON log(duration)');
+    }
+  }
+
+  function disconnect() {
+    connection.close();
+    connection = null;
+  }
+
+  return log;
+}(imports.gi.Gda, config));
+
+const tracking = (function (global, GnomeSession, log) {
+
+  let display = null;
+
+  let activeWindow = null;
+
+  let presence = null;
+
+  let windowCallbackID = 0;
+
+  let titleCallbackID = 0;
+
+  let presenceCallbackID = 0;
+
+  function init() {
+    presence = GnomeSession.Presence();
+    display = global.display;
+  }
+
+  function enable() {
+    presenceCallbackID = presence.connect('notify::status', changeStatus);
+    windowCallbackID = display.connect('notify::focus-window', changeWindow);
+  }
+
+  function disable() {
+    if (activeWindow && titleCallbackID) {
+      activeWindow.disconnect(titleCallbackID);
+    }
+    titleCallbackID = 0;
+
+    display.disconnect(windowCallbackID);
+    presence.disconnect(presenceCallbackID);
+    windowCallbackID = 0;
+    presenceCallbackID = 0;
+  }
+
+  function changeStatus() {
+    log({
+      type: 'presence',
+      key: 'status',
+      value: ['idle', 'invisible', 'busy', 'available'][presence.status],
+    });
+
+    // Log the window when going out of idle state.
+    if (presence.status === 3) {
+      changeWindow();
+    }
+
+    // Reset the window focus callback.
+    if (windowCallbackID) {
+      display.disconnect(windowCallbackID);
+    }
+    windowCallbackID = display.connect('notify::focus-window', changeWindow);
+  }
+
+  function changeWindow() {
+    if (activeWindow && titleCallbackID) {
+      activeWindow.disconnect(titleCallbackID);
+    }
+    activeWindow = display.focus_window;
+    if (activeWindow) {
+      titleCallbackID = activeWindow.connect('notify::title', changeTitle);
+      changeTitle();
+    }
+  }
+
+  function changeTitle() {
+    const win = display.focus_window;
+    if (!win) { return; }
+
+    log({
+      type: 'window',
+      key: win.get_wm_class(),
+      value: win.get_title()
+    });
+  }
+
+  return {
+    init: init,
+    enable: enable,
+    disable: disable
+  };
+}(global, imports.misc.gnomeSession, logData));
+
 
 function init() {
-  filePath = GLib.get_user_data_dir() + '/.windowtracking.log';
-  logFile = Gio.File.new_for_path(filePath);
-  logFileStream = logFile.append_to(Gio.FileCreateFlags.NONE, null);
-  presence = GnomeSession.Presence();
+  tracking.init();
 }
 
 function enable() {
-  presence.connectSignal('StatusChanged', onPresenceChange);
-  focusCallbackID = global.display.connect('notify::focus-window', onWindowChange);
+  tracking.enable();
 }
 
 function disable() {
-  global.display.disconnect(focusCallbackID);
-  logFileStream.close();
-  focusCallbackID = 0;
+  tracking.disable();
 }
